268b752497651428d5d8ca0060be2415c6fcb880
Shantanu Paul <122769152+shantanupaul81@users.noreply.github.com>
2023-11-29 20:34:11 +0530

    Update cartAction.js

--- src/redux/actions/cartAction.js
+++ src/redux/actions/cartAction.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../constants/cartConstant.js'
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://flipkartserver-k5mi.onrender.com';
 
 export const addToCart = (id, quantity) => async (dispatch) => {
     try { 
