d1583397b08b39a0164e9182282cc6823773fd4e
Shantanu Paul <122769152+shantanupaul81@users.noreply.github.com>
2023-11-29 20:33:11 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = 'https://flipkartserver-k5mi.onrender.com';
 
 export const authenticateSignup = async (data) => {
     try {
